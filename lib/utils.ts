6d3cf38edc4561fdfd14878b94926ae18bb58723
joeljanson <joel.janson.johansen@gmail.com>
2025-10-29 11:01:08 +0100

    fixed failing function

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,7 @@ export const getRelativeTime = (dateString: string) => {
 	const now = new Date();
 	const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
 
-	if (seconds < 60) return `${seconds}seconds ago`;
+	if (seconds < 60) return `${seconds}s ago`;
 	const minutes = Math.floor(seconds / 60);
 	if (minutes < 60) return `${minutes}m ago`;
 	const hours = Math.floor(minutes / 60);
